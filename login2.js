e24fda7deedee58d4a454163af742936a51372d5
Dev Shakya <shakyatechnicals@gmail.com>
2021-09-26 19:36:07 +0530

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,19 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="l","kira","light","Light", "L", " Kira"&& password=="bypass")
+{     
+
+    window.open("project-e.html");
+    return;
+    
+ 
+}
+else
+{
+    alert("Login failed");
+    window.open("error.html");
+}
+
+}
